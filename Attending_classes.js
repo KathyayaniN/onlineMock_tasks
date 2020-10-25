51c99674669b5adfa831fdc60fcc32ea4aa1a8a7
KathyayaniN <kathyayanivasanthi123@gmail.com>
2020-10-25 13:25:32 +0530

    first commit

---
+++ Attending_classes.js
@@ -0,0 +1,29 @@
+// let optional_class_schedule = [[2, 10], [11, 18],[1,3]]
+// let class_schedule = [[9, 17], [10, 12]]
+let optional_class_schedule = [[1603224000, 1603245600],[1603288800,1603303200]]
+let class_schedule = [[1603195200, 1603209600], [1603267200, 1603292400]]
+optional_classes(class_schedule, optional_class_schedule);
+function optional_classes(class_shd, optional_shd) {
+    let opt_clas = [];
+    for (let i = 0; i < optional_shd.length; i++) {
+        let opt_class_start = optional_shd[i][0];
+        let opt_class_end = optional_shd[i][1];
+        for(let j=i;j<class_shd.length;j++){
+            let schd_class_start = class_shd[j][0];
+            let schd_class_end = class_shd[j][1];
+            
+            if((opt_class_start < schd_class_start) && (opt_class_start <= schd_class_end) ){
+                if(opt_class_end < schd_class_start){
+                    opt_clas.push([opt_class_start,opt_class_end])
+                    break
+                }
+            }
+            if((opt_class_start > schd_class_start) && (opt_class_start >= schd_class_end)){
+                opt_clas.push([opt_class_start,opt_class_end])
+                break
+            }
+        }
+            
+    }
+    console.log(opt_clas)
+}
---
+++ Balanced_Brackets.js
@@ -0,0 +1,28 @@
+let n = 3; 
+Generating_balanced_brackets(n);
+function Generating_balanced_brackets(n){
+    let i = 0;
+    let open_bracket = 0;
+    let close_bracket = 0;
+    let str = []
+    let final_arr = recursive_fun(i,n,open_bracket,close_bracket,str);
+    console.log(final_arr.join(''))
+}
+function recursive_fun(pos,n,open,close,str){
+    if(close == n){
+        console.log(str.join(''))
+        return str;
+    }
+    else{
+        if(open > close){
+            str[pos] =')';
+            recursive_fun(pos+1,n,open,close+1,str);
+        }
+        if(open < n ){
+            str[pos]='(';
+            recursive_fun(pos+1,n,open+1,close,str);
+        }
+    }
+    // console.log(str)
+    return str;
+}
