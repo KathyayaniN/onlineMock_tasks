9de6c8a627f0e5404f75a5d96537544d877b7e40
Kathyayani Nalluri <58728148+KathyayaniN@users.noreply.github.com>
2020-10-25 13:47:06 +0530

    Update Attending_classes.js

--- Attending_classes.js
+++ Attending_classes.js
@@ -1,6 +1,7 @@
 // let optional_class_schedule = [[10/20/20 20pm, 10/21/20 14pm], [10/21/20 2am, 10/21/20 18pm]]
 // let class_schedule = [[10/20/20 12pm, 10/21/20 8am], [10/20/20 16pm, 10/21/20 12pm]]
 // These are arrays i have taken 
+// I have consider date and time in epoc time 
 let optional_class_schedule = [[1603224000, 1603245600],[1603288800,1603303200]]
 let class_schedule = [[1603195200, 1603209600], [1603267200, 1603292400]]
 optional_classes(class_schedule, optional_class_schedule);
